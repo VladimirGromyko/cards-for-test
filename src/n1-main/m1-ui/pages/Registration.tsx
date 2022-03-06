22987747be48b64c34fa0d813dc6b9b9a37d71e3
Pavel Laparevich <laparevichpavel@gmail.com>
2022-03-06 10:44:27 +0300

    added local state with functions

--- src/n1-main/m1-ui/pages/Registration.tsx
+++ src/n1-main/m1-ui/pages/Registration.tsx
@@ -1,18 +1,34 @@
-import React, { useState } from 'react'
+import React, { ChangeEvent, useState } from 'react'
 import SuperButton from '../common/c1-SuperButton/SuperButton'
 
 const Registration =() => {
+    const [email, setEmail] = useState('')
+    const [pass1, setPass1] = useState('')
+    const [pass2, setPass2] = useState('')
 
+    const onEmailChange = (e:ChangeEvent<HTMLInputElement>) => {
+        setEmail(e.currentTarget.value)
+    }
+    const onPass1Change = (e:ChangeEvent<HTMLInputElement>) => {
+        setPass1(e.currentTarget.value)
+    }
+    const onPass2Change = (e:ChangeEvent<HTMLInputElement>) => {
+        setPass2(e.currentTarget.value)
+    }
+
+    const onRegisterClick = () => {
+        alert([email, pass1, pass2])
+    }
 
     return (
         <div>
             <span>Email</span>
-            <input />
+            <input onChange={onEmailChange}/>
             <span>Password</span>
-            <input />
+            <input onChange={onPass1Change}/>
             <span>Second Password</span>
-            <input />
-            <SuperButton>Register</SuperButton>
+            <input onChange={onPass2Change}/>
+            <SuperButton onClick={onRegisterClick}>Register</SuperButton>
         </div>
     )
 }
