0e4f2f837af4d6e8a05697660bf3c430631df9ea
Lapa17 <laparevichpavel@gmail.com>
2022-03-19 14:48:40 +0300

    fixed html

--- src/n1-main/m1-ui/pages/MainPage/PackList/PacksPage.tsx
+++ src/n1-main/m1-ui/pages/MainPage/PackList/PacksPage.tsx
@@ -1,13 +1,13 @@
-import { useDispatch, useSelector } from "react-redux";
-import { NavLink, useNavigate } from "react-router-dom";
-import React, { useCallback } from "react";
+import {useDispatch, useSelector} from "react-redux";
+import {NavLink, useNavigate} from "react-router-dom";
+import React, {useCallback} from "react";
 import commonPacksStyle from "./PacksPage.module.css"
 import SuperInputText from "../../../common/c2-SuperInput/SuperInputText";
-import { PacksTable } from "./PacksTable";
-import { AppStoreType } from "../../../../m2-bll/store";
+import {PacksTable} from "./PacksTable";
+import {AppStoreType} from "../../../../m2-bll/store";
 import Sidebar from "./Sidebar";
-import { HeaderPacks } from "./HeaderPacks";
-import { PATH } from "../../../routes/Paths";
+import {HeaderPacks} from "./HeaderPacks";
+import {PATH} from "../../../routes/Paths";
 import l from "../../../common/c7-Loading/loader07.module.css";
 import SuperButton from "../../../common/c1-SuperButton/SuperButton";
 import {
@@ -20,11 +20,11 @@ import {
     showEditPackAC
 } from "../../../../m2-bll/packsReducer";
 import {PacksGetResponseDataType} from "../../../../m3-dal/packs-api";
-import { CardPacksType } from "../../../../m3-dal/packs-api";
+import {CardPacksType} from "../../../../m3-dal/packs-api";
 import s from '../../../header/header.module.css';
-import { ResponseErrorStateType } from "../../../../m2-bll/errorReducer";
-import { errorResponse } from "../../../../../n2-features/f0-test/errorResponse";
-import { AddPack } from "./AddPack";
+import {ResponseErrorStateType} from "../../../../m2-bll/errorReducer";
+import {errorResponse} from "../../../../../n2-features/f0-test/errorResponse";
+import {AddPack} from "./AddPack";
 
 
 export const PacksPage = () => {
@@ -48,10 +48,10 @@ export const PacksPage = () => {
         state.packs.isShownDeletePack)
 
     const pickedEditPack = useSelector<AppStoreType, { packName: string, packId: string }>
-        ((state: AppStoreType) => state.packs.pickedEditPack)
+    ((state: AppStoreType) => state.packs.pickedEditPack)
 
     const pickedDeletePack = useSelector<AppStoreType, { packName: string, packId: string }>
-        ((state: AppStoreType) => state.packs.pickedDeletePack)
+    ((state: AppStoreType) => state.packs.pickedDeletePack)
     // pickedDeletePack)
 
     const dispatch = useDispatch()
@@ -83,10 +83,10 @@ export const PacksPage = () => {
 
     const addPackList = useCallback(() => {
         dispatch(showAddPackAC(true))
-    },[dispatch])
+    }, [dispatch])
 
     const addPack = useCallback((pack: string) => {
-        dispatch(addPacksTC({ cardsPack: { name: pack } }))
+        dispatch(addPacksTC({cardsPack: {name: pack}}))
         // dispatch(showEditPackAC(true))
         // console.log('New pack: ', pack)
     }, [dispatch,])
@@ -113,7 +113,7 @@ export const PacksPage = () => {
     }, [dispatch])
 
     const editPack = useCallback((packId: string, namePack: string) => {
-        dispatch(editPackTC({ cardsPack: { _id: packId, name: namePack } }))
+        dispatch(editPackTC({cardsPack: {_id: packId, name: namePack}}))
         // dispatch(showEditPackAC(true))
     }, [dispatch])
 
@@ -133,7 +133,7 @@ export const PacksPage = () => {
 
     return (
         <div className={commonPacksStyle.wrapper}>
-            <div style={{ width: '100%' }}>
+            <div style={{width: '100%'}}>
                 {isLoading === "loading" && <div className={l.loader07}></div>}
             </div>
             <nav>
@@ -156,42 +156,44 @@ export const PacksPage = () => {
                         <SuperButton onClick={onSetAllPressHandler}>All cardPacks</SuperButton>
                         <SuperButton onClick={onSetMyPressHandler}>My cardPacks</SuperButton>
                     </div>
-                    <div style={{ color: 'red' }}>
+                    <div style={{color: 'red'}}>
                         {errorResponse(errorRes, 'setPacks')}
                     </div>
                 </div>
                 <div className={commonPacksStyle.content}>
-                    <Sidebar />
-                </div>
-                <div className={commonPacksStyle.content}>
-                    <div>Packs</div>
-                    <SuperInputText placeholder='Enter cardPacks name for searching'/>
-                    <div><SuperButton onClick={addPackList}>Add new pack</SuperButton></div>
-                    {isShownAddPack && <AddPack
-                        addPack={addPack}
-                        hideAddPack={hideAddPack}
-                        isLoading={isLoading}/>}
-                    <HeaderPacks/>
-                    {packs && !isShownAddPack && <PacksTable
-                        deletePack={deletePack}
-                        deletePackList={deletePackList}
-                        hideDeletePack={hideDeletePack}
-                        deletePackId={pickedDeletePack.packId}
-                        deletePackName={pickedDeletePack.packName}
-                        editPack={editPack}
-                        editPackList={editPackList}
-                        hideEditPack = {hideEditPack}
-                        packId={pickedEditPack.packId}
-                        packName={pickedEditPack.packName}
-                        learnPack={learnPack}
-                        packs={packs}
-                        isLoading={isLoading}
-                        isShownEditPack={isShownEditPack}
-                        isShownDeletePack={isShownDeletePack}
-                        currentPage={currentPage}
-                        onPageChanged={onPageChanged}
-                    />}
+                    <Sidebar/>
                 </div>
             </div>
+            <div className={commonPacksStyle.content}>
+                <div>Packs</div>
+                <SuperInputText placeholder='Enter cardPacks name for searching'/>
+                <div><SuperButton onClick={addPackList}>Add new pack</SuperButton></div>
+                {isShownAddPack && <AddPack
+                    addPack={addPack}
+                    hideAddPack={hideAddPack}
+                    isLoading={isLoading}/>}
+                <HeaderPacks/>
+                {packs && !isShownAddPack && <PacksTable
+                    deletePack={deletePack}
+                    deletePackList={deletePackList}
+                    hideDeletePack={hideDeletePack}
+                    deletePackId={pickedDeletePack.packId}
+                    deletePackName={pickedDeletePack.packName}
+                    editPack={editPack}
+                    editPackList={editPackList}
+                    hideEditPack={hideEditPack}
+                    packId={pickedEditPack.packId}
+                    packName={pickedEditPack.packName}
+                    learnPack={learnPack}
+                    packs={packs}
+                    isLoading={isLoading}
+                    isShownEditPack={isShownEditPack}
+                    isShownDeletePack={isShownDeletePack}
+                    currentPage={currentPage}
+                    onPageChanged={onPageChanged}
+                />}
+            </div>
+        </div>
+
     )
 }
