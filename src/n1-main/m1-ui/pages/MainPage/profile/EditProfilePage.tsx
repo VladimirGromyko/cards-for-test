eff883342e8ff4a302701d76b73c4d60f9d0b722
DinaSem <neispravimayas1@gmail.com>
2022-03-21 10:42:04 +0300

    design EditProfile

--- src/n1-main/m1-ui/pages/MainPage/profile/EditProfilePage.tsx
+++ src/n1-main/m1-ui/pages/MainPage/profile/EditProfilePage.tsx
@@ -5,7 +5,7 @@ import SuperButton from "../../../common/c1-SuperButton/SuperButton";
 import {useDispatch, useSelector} from "react-redux";
 import {AppStoreType} from "../../../../m2-bll/store";
 import {changeUserNameTC} from "../../../../m2-bll/auth-reducer";
-import { Navigate } from 'react-router-dom';
+import {Navigate} from 'react-router-dom';
 import {PATH} from "../../../routes/Paths";
 
 
@@ -27,25 +27,31 @@ export const EditProfilePage = () => {
     }, [userName])
 
     if (!isLoggedIn) {
-        return <Navigate to={PATH.LOGIN} />
+        return <Navigate to={PATH.LOGIN}/>
     }
 
     return (
         <div className={s.wrapper}>
-            <h3>Personal Information</h3>
-            <img className={s.photo}
-                 src="https://steamcdn-a.akamaihd.net/steamcommunity/public/images/avatars/c7/c7caa60f60d75f36e2b2567904bba2cca3cbf48c_full.jpg"
-                 alt="UserPhoto"/>
-            <div className={s.inputTitle}>Nickname</div>
-            <div>
-                <SuperInputText
-                    value={nameFromInput}
-                    onChangeText={setNameFromInput}
-                />
+            <div className={s.container}>
+                <h3>Personal Information</h3>
+                <img className={s.photo}
+                     src="https://steamcdn-a.akamaihd.net/steamcommunity/public/images/avatars/c7/c7caa60f60d75f36e2b2567904bba2cca3cbf48c_full.jpg"
+                     alt="UserPhoto"/>
+                <div className={s.containerForEmail}>
+                    <div className={s.textField}>Nickname</div>
+                    <div>
+                        <SuperInputText
+                            value={nameFromInput}
+                            onChangeText={setNameFromInput}
+                        />
+                    </div>
+                </div>
+                <div className={s.containerForPassword}>
+                    <div className={s.textField}>Email</div>
+                    <div><SuperInputText value={userEmail}/></div>
+                </div>
+                <div className={s.button}><SuperButton onClick={onChangeNameHandler}>SAVE</SuperButton></div>
             </div>
-            <div className={s.inputTitle}>Email</div>
-            <div><SuperInputText value={userEmail}/></div>
-            <div className={s.button}><SuperButton onClick={onChangeNameHandler}>SAVE</SuperButton></div>
         </div>
     )
 }
